f563af49dae6abd7e8ba2c2ebb0b6445a2736071
rafael <rafaelddem@gmail.com>
2024-05-01 23:22:29 -0300

    Refeito seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            OwnerSeeder::class, 
+            OwnerSeeder::class,
             WalletSeeder::class, 
-            PaymentMethodSeeder::class, 
-            MovementTypeSeeder::class, 
-
-            MovementSeeder::class,
         ]);
     }
 }
--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -16,34 +16,20 @@ class OwnerSeeder extends Seeder
     {
         // id: 1
         DB::table('owners')->insert([
-            'name' => 'Não definida', 
-            'active' => true, 
-            'created_at' => now(), 
-            'updated_at' => now(), 
+            'id' => 1,
+            'name' => 'Sistema',
+            'active' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         // id: 2
         DB::table('owners')->insert([
-            'name' => 'Rafael', 
-            'active' => true, 
-            'created_at' => now(), 
-            'updated_at' => now(), 
-        ]);
-
-        // id: 3
-        DB::table('owners')->insert([
-            'name' => 'Terezinha', 
-            'active' => true, 
-            'created_at' => now(), 
-            'updated_at' => now(), 
-        ]);
-
-        // id: 4
-        DB::table('owners')->insert([
-            'name' => 'Márcio', 
-            'active' => true, 
-            'created_at' => now(), 
-            'updated_at' => now(), 
+            'id' => 2,
+            'name' => 'Rafael',
+            'active' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
