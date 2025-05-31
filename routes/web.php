e59b68adb7eea750ecd2be053376c63e674992ff
rafaelddem <rafaelddem@gmail.com>
2025-05-31 19:58:50 +0000

    Removido comentario do web.php

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\TransactionTypeController;
 use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {return view('index');});
 
 Route::group(['prefix' => 'dono', 'as' => 'owner.'], function () {
